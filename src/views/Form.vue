0cf57baf34e279c35d173301e0961b99c3618b8e
lm <15090020990@163.com>
2021-02-01 15:41:46 +0800

    title

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,25 +1,26 @@
 <template>
-    <el-form  :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-       <div class="flex-form">
-           <el-form-item class="name">
-               <el-input placeholder="First Name" v-model="ruleForm.first"></el-input>
-           </el-form-item>
-           <el-form-item class="name">
-               <el-input placeholder="Last Name" v-model="ruleForm.last"></el-input>
-           </el-form-item>
-       </div>
-        <el-form-item class="tp">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <div class="flex-form">
+            <el-form-item class="name" prop="first">
+                <el-input placeholder="First Name" v-model="ruleForm.first"></el-input>
+            </el-form-item>
+            <el-form-item class="name" prop="last">
+                <el-input placeholder="Last Name" v-model="ruleForm.last"></el-input>
+            </el-form-item>
+        </div>
+        <el-form-item class="tp" prop="email">
             <el-input placeholder="Email" v-model="ruleForm.email"></el-input>
         </el-form-item>
-        <el-form-item  class="tp">
+        <el-form-item class="tp" prop="company">
             <el-input placeholder="Company" v-model="ruleForm.company"></el-input>
         </el-form-item>
-        <el-form-item  class="tp">
+        <el-form-item class="tp" prop="birefly">
             <el-input type="textarea" placeholder="Birefly" v-model="ruleForm.birefly"></el-input>
         </el-form-item>
 
         <el-form-item>
-            <el-button class="submitForm" type="" @click="submitForm('ruleForm')">Send Message</el-button>
+            <el-button :loading="loading" class="submitForm" type="" @click="submitForm('ruleForm')">Send Message
+            </el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -38,29 +39,44 @@ export default {
                 birefly: '',
             },
             rules: {
-
-            }
+                first: [{required: true, message: 'First name', trigger: 'change'}],
+                last: [{required: true, message: 'Last name', trigger: 'change'}],
+                email: [{required: true, message: 'Email', trigger: 'change'}],
+                company: [{required: true, message: 'Company', trigger: 'change'}],
+            },
+            loading: false,
+            timer: null
         };
     },
     methods: {
         submitForm(formName) {
+            console.log(formName);
             this.$refs[formName].validate((valid) => {
+                console.log(valid);
                 if (valid) {
+                    this.loading = true;
+                    this.timer = setTimeout(() => {
+                        this.loading = false;
+                    }, 3000);
 
                 } else {
                     return false;
                 }
             });
         },
+    },
+    destroyed() {
+        clearTimeout(this.timer);
     }
 };
 </script>
 
 <style lang="less" scoped>
-.demo-ruleForm{
+.demo-ruleForm {
     width: 642PX;
     height: 459PX;
-    .flex-form{
+
+    .flex-form {
         display: flex;
         justify-content: space-between;
     }
